a6337fc6949b34f4866d8bfb886fa0f720734a73
Sam Shull <samuel.c.shull@gmail.com>
2015-10-01 15:03:09 -0400

    Basic main page with links and buttons set up

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,13 +1,51 @@
+// Button handles (global)
+var $left
+var $right
+
+// "Slide" handles (global)
+var $welcome
+var $distance
+
 $(document).ready(function() {
   console.log("JS ready");
 
-  // Easy handles on the various UI slides
-  var $welcome = $('#welcome-slide');
-  var $distance = $('#distance-slide');
+  // Set easy handles on UI buttons
+  $left = $('#left');
+  $right = $('#right');
 
+
+  // Set easy handles on the various UI slides
+  $welcome = $('#welcome-slide');
+  $distance = $('#distance-slide');
+
+  // Listeners for welcome slide links..
   $('#launch-link').on('click', function(e) {
     e.preventDefault();
     $welcome.hide();
-    $distance.show();
+    displayDistance();
   });
-});
+
+});   // Closing document ready
+
+
+// *****  Functions for Displaying various "slides" within user interface *****
+
+// Function for displaying Welcome slide...
+
+function displayWelcome() {
+  $welcome.show();
+  $left.hide();
+  $right.hide();
+}
+
+// Function for displaying Distance Query slide...
+
+function displayDistance() {
+  $distance.show();
+  $left.show();
+  $right.show();
+  $left.on('click', function() {
+    $distance.hide();
+    displayWelcome();
+  });
+}
