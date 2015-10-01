e83553f28c6eb004087d3b0592b2cbed342fcb04
Sam Shull <samuel.c.shull@gmail.com>
2015-10-01 17:20:02 -0400

    Third slide added and js code cleaned up

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,22 +1,28 @@
-// Button handles (global)
-var $left
-var $right
 
-// "Slide" handles (global)
-var $welcome
-var $distance
 
 $(document).ready(function() {
+
+  var myApp = myApp || {};
+
   console.log("JS ready");
+
+  // Button handles (global)
+  var $left;
+  var $right;
+
+  // "Slide" handles (global)
+  var $welcome;
+  var $distance;
+  var $days;
 
   // Set easy handles on UI buttons
   $left = $('#left');
   $right = $('#right');
 
-
   // Set easy handles on the various UI slides
   $welcome = $('#welcome-slide');
   $distance = $('#distance-slide');
+  $days = $('#days-slide');
 
   // Listeners for welcome slide links..
   $('#launch-link').on('click', function(e) {
@@ -25,27 +31,44 @@ $(document).ready(function() {
     displayDistance();
   });
 
+  // *****  Functions for Displaying various "slides" within user interface *****
+
+  // Function for displaying Welcome slide...
+
+  function displayWelcome() {
+    $welcome.show();
+    $left.hide();
+    $right.hide();
+  }
+
+  // Function for displaying Distance Query slide...
+
+  function displayDistance() {
+    $distance.show();
+    $left.show();
+    $right.show();
+    $left.on('click', function() {
+      $distance.hide();
+      $left.off();
+      $right.off();
+      displayWelcome();
+    });
+    $right.on('click', function() {
+      $distance.hide();
+      $left.off();
+      $right.off();
+      displayDays();
+    });
+  }
+
+  function displayDays() {
+    $days.show();
+    $left.show();
+    $right.show();
+    $left.on('click', function() {
+      $days.hide();
+      displayDistance();
+    });
+  }
+
 });   // Closing document ready
-
-
-// *****  Functions for Displaying various "slides" within user interface *****
-
-// Function for displaying Welcome slide...
-
-function displayWelcome() {
-  $welcome.show();
-  $left.hide();
-  $right.hide();
-}
-
-// Function for displaying Distance Query slide...
-
-function displayDistance() {
-  $distance.show();
-  $left.show();
-  $right.show();
-  $left.on('click', function() {
-    $distance.hide();
-    displayWelcome();
-  });
-}
