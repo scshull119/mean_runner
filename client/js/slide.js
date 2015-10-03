fe3a2c5c0e5cceec5bd144f3d9849dddb285e668
Sam Shull <samuel.c.shull@gmail.com>
2015-10-03 00:20:56 -0400

    slide logic refactored

---
+++ client/js/slide.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+
+  var myApp = myApp || {};
+
+  console.log("JS ready");
+
+  // Set easy handles on UI buttons
+  var $left = $('#left');
+  var $right = $('#right');
+
+  // Set easy handles on the various UI slides
+  var $welcome = $('#welcome-slide');
+  var $distance = $('#distance-slide');
+  var $days = $('#days-slide');
+  var $pace = $('#pace-slide');
+  var $signup = $('#signup-slide');
+  var $login = $('#login');
+
+  // Listeners for welcome slide links..
+  $('#launch-link').on('click', function(e) {
+    e.preventDefault();
+    slideOut($welcome, 'left');
+    displaySlide(newOrder, 1, 'right');
+  });
+
+  $('#login-link').on('click', function(e) {
+    e.preventDefault();
+    slideOut($welcome, 'left');
+    displaySlide(loginOrder, 1, 'right');
+  });
+
+  // *****  Functions for Displaying various "slides" within user interface *****
+
+  // Function for displaying Welcome slide...
+
+  function displayWelcome(origin) {
+    slideIn($welcome, origin);
+    $left.hide();
+    $right.hide();
+  }
+
+// Function and arrays for maniging slide order and forward/back controls.
+
+  var newOrder = [$welcome, $distance, $days, $pace, $signup];
+  var loginOrder = [$welcome, $login];
+
+  function displaySlide(orderArray, index, origin) {
+    $slide = orderArray[index];
+    slideIn($slide, origin);
+
+    if(index > 1) {
+      $left.show();
+      $left.on('click', function() {
+        slideOut($slide, 'right');
+        $left.off();
+        $right.off();
+        displaySlide(orderArray, (index - 1), 'left');
+      });
+
+    } else if(index === 1) {
+      $left.show();
+      $left.on('click', function() {
+        slideOut($slide, 'right');
+        $left.off();
+        $right.off();
+        displayWelcome('left');
+      });
+
+    } else {
+      $left.off();
+      $left.hide();
+    }
+
+    if(index < (orderArray.length - 1)) {
+      $right.show();
+      $right.on('click', function() {
+        slideOut($slide, 'left');
+        $left.off();
+        $right.off();
+        displaySlide(orderArray, (index + 1), 'right');
+      });
+
+    } else {
+      $right.off();
+      $right.hide();
+    }
+  }
+
+  // Functions for moving slides in and out to/from the left and right
+
+  function slideIn($slide, direction) {
+    console.log("Sliding in from " + direction + "!");
+
+    var positionValue;
+
+    if(direction === "left") {
+      positionValue = '-100%';
+    } else if(direction === "right") {
+      positionValue = '100%';
+    }
+
+    $slide.css('left', positionValue);
+    var slideObject = {};
+    slideObject['left'] = '0';
+    $slide.animate(slideObject);
+  }
+
+  function slideOut($slide, direction) {
+
+    var positionValue;
+
+    if(direction === "left") {
+      positionValue = '-100%';
+    } else if(direction === "right") {
+      positionValue = '100%';
+    }
+
+    console.log("Sliding out to " + direction + "!");
+    var slideObject = {};
+    slideObject['left'] = positionValue;
+    $slide.animate(slideObject, function() {
+      console.log("Slide out COMPLETED");
+    });
+  }
+
+});   // Closing document ready
